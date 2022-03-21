5c4fec2136dc2bc031c506c777182b2f7af106c5
boysangurkha <r0797383@student.thomasmore.be>
2022-03-21 18:00:58 +0100

    Simpel Login en registreer forms aangemaakt in html

---
+++ register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - Imageo</title>
+</head>
+<body>
+    <div id="registerForm">
+        <form method="post" action=>
+            <input name="email" placeholder="Email" type="email" required autofocus />
+            <input name="username" placeholder="Username" type="password" required />
+            <input name="password" placeholder="Password" type="password" required />
+            <input name="password_conf" placeholder="Confirm Password" type="password" required />
+            <input name="register" type="submit" value="Register" />
+        </form>
+    </div>
+</body>
+</html>
