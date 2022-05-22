23c50e510814f73a544d08725d8c369629da581d
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-05-22 20:01:17 +0200

    feat: added necessary functions for post class

--- classes/post.php
+++ classes/post.php
@@ -2,7 +2,129 @@
     include_once(__DIR__ . "/db.php");
 
     class Post {
-        private $user_email;
+      
         private $title;
+        private $description;
+        private $userId;
+        private $image;
+        private $createdAt;
+
+        public function getTitle()
+        {
+                return $this->title;
+        }
+
+        public function setTitle($title)
+        {
+            if(empty($title)){
+                throw new Exception("Title cannot be empty.");
+            }
+            $this->title = $title;
+
+            return $this;
+        }
+
+        public function getDescription()
+        {
+                return $this->description;
+        }
+
+        public function setDescription($description)
+        {
+            if(empty($description)){
+                throw new Exception("Description cannot be empty.");
+            }
+            $this->description = $description;
+
+            return $this;
+        }
+
+        public function getUserId()
+        {
+                return $this->userId;
+        }
+
+        public function setUserId($userId)
+        {
+            $this->userId = $userId;
+
+            return $this;
+        }
+
+        public static function getAll()
+        {
+            $conn = Db::getInstance();
+            $stmt = $conn->prepare("SELECT * FROM posts");
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            return $result;
+        }
+
+        public static function getById($id)
+        {
+            $conn = Db::getInstance();
+            $stmt = $conn->prepare("SELECT * FROM posts WHERE id = :id");
+            $stmt->bindValue(":id", $id);
+            $stmt->execute();
+            $result = $stmt->fetch();
+            return $result;
+        }
+
+        public static function getByUserId($userId)
+        {
+            $conn = Db::getInstance();
+            $stmt = $conn->prepare("SELECT * FROM posts WHERE user_id = :user_id");
+            $stmt->bindValue(":user_id", $userId);
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            return $result;
+        }
+
+        public static function deleteById($id)
+        {
+            $conn = Db::getInstance();
+            $stmt = $conn->prepare("DELETE FROM posts WHERE id = :id");
+            $stmt->bindValue(":id", $id);
+            $stmt->execute();
+        }
+
+        public function getImage()
+        {
+            return $this->image;
+        }
+
+        public function setImage($image)
+        {
+            $this->image = $image;
+
+            return $this;
+        }
+
+        public function getCreatedAt()
+        {
+            return $this->createdAt;
+        }
+
+        public function setCreatedAt($createdAt)
+        {
+            $this->createdAt = $createdAt;
+
+            return $this;
+        }
+
+        //save
+
+        public function save()
+        {
+            $conn = Db::getInstance();
+            $stmt = $conn->prepare("INSERT INTO posts (title, description, user_id, image, created_at) VALUES (:title, :description, :user_id, :image, :created_at)");
+            $stmt->bindValue(":title", $this->title);
+            $stmt->bindValue(":description", $this->description);
+            $stmt->bindValue(":user_id", $this->userId);
+            $stmt->bindValue(":image", $this->image);
+            $stmt->bindValue(":created_at", $this->createdAt);
+            $stmt->execute();
+        }
+
 
     }
