e90797e80e77069ab722b58800c0eed944d11fc3
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-05-28 15:13:14 +0200

    style: added styling to add gallery page

--- gallery.php
+++ gallery.php
@@ -22,29 +22,39 @@ $user = User::getUserByEmail($_SESSION['email']);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Imageo - <?php echo htmlspecialchars($user['username']);?></title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/button.css">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="./css/button.css">
+    <?php include_once(__DIR__ . "/helpers/fonts.php")?>
     <style>
-        .gallery_upload input::placeholder{
-            color: black;
-        }
-        .gallery_upload{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: row;
-        }
+      
+        
     </style>
 </head>
 <body>
     <?php include_once(__DIR__ . "/partials/nav.php")?>
-    Gallery
+
   
-    <form class="gallery_upload" action="helpers/gallery-upload.php" method="POST" enctype="multipart/form-data">
-    <p><input type="text" name="galleryTitle" placeholder="Project title..."></p>
-    <p><input type="text" name="galleryDesc" placeholder="Project description..."></p>
-    <p><input type="file" name="images[]" value='' multiple></p>
-    <p><input type="submit" name="submit" value="Upload"></p>
+    <form class="gallery_upload Form" action="helpers/gallery-upload.php" method="POST" enctype="multipart/form-data">
+    <h1>Gallery</h1>
+   <div class="form-group">
+       <input class="form-input ani" type="text" name="galleryTitle" placeholder="Project title...">
+       <span class="border-bottom-animation left"></span>
+    </div>
+            
+    <div class="form-group"><input class="form-input ani" type="text" name="galleryDesc" placeholder="Project description..."><span class="border-bottom-animation left"></span></div>
+    <div class="form-group"><input class="form-input ani" type="file" name="images[]" value='' multiple><span class="border-bottom-animation left"></span></div>
+   
+    <div class="container">
+                <div class="center">
+                    <button class="btn" name="submit" type="submit" value="Upload">
+                        <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+                        <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+                        <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+                        </svg>
+                        <span id="loginBtn">Upload</span>
+                    </button>
+                </div>
+            </div>
     </form>
 
     <!-- go back link -->
