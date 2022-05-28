336c63febbdc23589b9b1e064a7e30495b49fd79
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-05-28 14:21:02 +0200

    feat: prepared page to add new projects

--- gallery.php
+++ gallery.php
@@ -22,17 +22,33 @@ $user = User::getUserByEmail($_SESSION['email']);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Imageo - <?php echo htmlspecialchars($user['username']);?></title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/button.css">
+    <style>
+        .gallery_upload input::placeholder{
+            color: black;
+        }
+        .gallery_upload{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: row;
+        }
+    </style>
 </head>
 <body>
     <?php include_once(__DIR__ . "/partials/nav.php")?>
     Gallery
   
-    <form action="helpers/gallery-upload.php" method="POST" enctype="multipart/form-data">
-    <input type="text" name="galleryTitle" placeholder="Project title...">
-    <input type="text" name="galleryDesc" placeholder="Project description...">
-    <input type="file" name="images[]" value='' multiple>
-    <input type="submit" name="submit" value="Upload">
+    <form class="gallery_upload" action="helpers/gallery-upload.php" method="POST" enctype="multipart/form-data">
+    <p><input type="text" name="galleryTitle" placeholder="Project title..."></p>
+    <p><input type="text" name="galleryDesc" placeholder="Project description..."></p>
+    <p><input type="file" name="images[]" value='' multiple></p>
+    <p><input type="submit" name="submit" value="Upload"></p>
     </form>
+
+    <!-- go back link -->
+    <a href="index.php">Go back</a>
 
 
 </body>
